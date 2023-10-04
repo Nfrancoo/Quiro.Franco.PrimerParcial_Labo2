555e41f76417f101141580169215d72e3cb69a1c
Nfrancoo <francotobiasquiro@gmail.com>
2023-10-04 14:28:16 -0300

    PrimerParcial

---
+++ PrimerParcial/CodeFile1.cs
@@ -0,0 +1,7 @@
+﻿public enum EPosicion
+{
+    Arquero,
+    Defensa,
+    Mediocentro,
+    Delantero
+}
--- PrimerParcial/JugadoresSeleccion.cs
+++ PrimerParcial/JugadoresSeleccion.cs
@@ -2,6 +2,12 @@
 {
     public class JugadoresSeleccion
     {
+        public int edad;
+        public string nombre;
+        public string apellido;
+        
+
+
 
     }
 }
